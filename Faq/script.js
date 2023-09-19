da385d1e52f5700ce0dfa9c8ddd82cb420c92e24
abhay395 <140682257+abhay395@users.noreply.github.com>
2023-09-19 17:23:10 +0530

    complete

--- Faq/script.js
+++ Faq/script.js
@@ -1,29 +1,13 @@
-// const plusbtn=document.querySelector('.plus-icon')
-// const minusbtn=document.querySelector('.minus-icon')
-// const qt=document.querySelector('.question-text')
-// plusbtn.addEventListener('click',function(){
-//     qt.style.display='block'
-//     plusbtn.style.display='none'
-//     minusbtn.style.display='block'
-
-// })
-// minusbtn.addEventListener('click',function(){
-//     qt.style.display='none'
-//     plusbtn.style.display='block'
-//     minusbtn.style.display='none'
-// })
-// const plusbtn=document.querySelector('.plus-icon')
-// const btn=document.querySelector('.question-btn')
-// const minusbtn=document.querySelector('.minus-icon')
-// const qt=document.querySelector('.question-text')
-// plusbtn.addEventListener('click',function(){
-//    btn.classList.add('show-text')
-//     qt.classList.add('show-text')
-// })
 const btns =document.querySelectorAll(".question-btn")
 const article =document.querySelector(".question")
 btns.forEach((btn)=>{
-    btn.addEventListener('click',function(){
-        btn.parentElement.parentElement.classList.toggle("show-text")
+    // btn.addEventListener('click',function(){
+    // const qustion=btn.parentElement.parentElement
+    // qustion.classList.toggle("show-text")
+    // })
+    // don sahi hai
+    btn.addEventListener('click',function(e){
+        const qustion=e.currentTarget.parentElement.parentElement
+        qustion.classList.toggle("show-text")
     })
 })
